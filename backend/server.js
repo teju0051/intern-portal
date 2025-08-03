fe86bc174391c4a7ae55249cc39a49567b5a3f1e
Mr. Tejas Shinde <82798163+teju0051@users.noreply.github.com>
2025-08-03 21:56:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-ss
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+app.use(cors()); 
+
+
+const internData = {
+  name: "Ashwini Panchal",
+  referralCode: "ashwini26",
+  donationsRaised: 261100
+};
+
+app.get('/api/intern', (req, res) => {
+  res.json(internData);
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
